d24a1ebbd7af9d8cbefc7ba0b0247a01c17a4e8f
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-02-25 14:32:24 +0700

    update search page ui

--- public/js/core.js
+++ public/js/core.js
@@ -1,11 +1,61 @@
-$(document).ready(init);
-function init() {}
+let scrollData = 0;
+let isSearchPageMode = false;
+$(document).ready(function () {
+  init();
+});
+function init() {
+  initEvent();
+}
 function initEvent() {
-  $("body").click(function (e) {
+  $(window).click(function (e) {
     e.preventDefault();
     eventHandler(e);
+  });
+  $(window).resize(function () {
+    responsive();
+  });
+  $(window).scroll(function () {
+    console.log($(document).scrollTop());
+    if (isSearchPageMode) return;
+    scrollData = $(document).scrollTop();
   });
 }
 function eventHandler(e) {
   console.log(e);
+  const target = $(e.target).attr("id");
+
+  if (target === "search-btn") {
+    enableSearchPage();
+    isSearchPageMode = true;
+  } else if (target === "search-cls-btn") {
+    disableSearchPage();
+    $(document).scrollTop(scrollData);
+    isSearchPageMode = false;
+  }
+}
+function responsive() {
+  if ($(window).width() > 640 && isSearchPageMode) {
+    disableSearchPage();
+    isSearchPageMode = false;
+  }
+}
+function enableSearchPage() {
+  $("#search-btn").parent().addClass("hidden");
+  $("#search").removeClass("hidden");
+  $("#search").addClass("sm:flex");
+  $("#search").addClass("flex");
+  $("#menu").addClass("hidden");
+  $("#main-content").addClass("hidden");
+  $("#menu-bottom").addClass("hidden");
+  $("#search-result").removeClass("hidden");
+}
+function disableSearchPage() {
+  $("#search-btn").parent().removeClass("hidden");
+  $("#search").addClass("hidden");
+  $("#search").removeClass("sm:flex");
+  $("#search").removeClass("flex");
+  $("#menu").removeClass("hidden");
+  $("#main-content").removeClass("hidden");
+  $("#menu-bottom").removeClass("hidden");
+  $("#search-result").addClass("hidden");
 }
